e0ecee81d885213a84c09778f2a9ef5ca5a89091
Angel Santana <angelash303@gmail.com>
2022-04-29 19:57:10 -0600

    Updates

---
+++ precache-manifest.9b87703f4abdc381579a81b80f64f6f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def0b23f99d31ae5d29c9d5fa67a4c2a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6f0945bc9fa8e74dbf0b",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "b9baa7e954e9c7514950",
+    "url": "/home/static/js/2.6bcb2a12.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.6bcb2a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0945bc9fa8e74dbf0b",
+    "url": "/home/static/js/main.52bdaae6.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "930f6cf63b97ba2e1f8a4bc61702fe48",
+    "url": "/home/static/media/angel_faceshot.930f6cf6.jpg"
+  },
+  {
+    "revision": "d2785869a3f69a2f993831559e455d21",
+    "url": "/home/static/media/resume.d2785869.pdf"
+  }
+]);
